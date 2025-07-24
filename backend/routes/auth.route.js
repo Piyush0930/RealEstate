75778559845eb390ee2d13c9b451e2309deb3095
piyush <piyushpatil922004@gmail.com>
2025-07-24 17:54:56 +0530

    add route '/' for server running

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -3,6 +3,10 @@ import { login, logOut, sighUp } from "../controllers/auth.controller.js"
 
 const authRouter = express.Router()
 
+authRouter.get("/", (req, res) => {
+  res.send("Auth API is running...");
+})
+
 authRouter.post("/signup",sighUp)
 authRouter.post("/login",login)
 authRouter.post("/logout",logOut)
